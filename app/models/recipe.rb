9d4ffb6712d41c1071540d0cdbba56f55ad2a39d
Jessica Moura De Sousa <jessicamoura15@gmail.com>
2016-02-11 16:16:41 -0200

    added validation to recipe fields

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :food_type
   belongs_to :preference
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :kitchen_id, :food_type_id, :preference_id, :ingredients, :step_by_step, presence: true
   has_many :favorite_recipes
   has_many :favorited_by, through: :favorite_recipes, source: :user
 
